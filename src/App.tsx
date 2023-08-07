6df60760043adc15a2675033e81d853ca4c4cf2a
曹珂俭 <1849201815@qq.com>
2023-08-07 15:26:33 +0800

    初始化React版本项目

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import s from './App.module.scss'
+function App() {
+  return (
+    <div>1
+      <svg className={s.svg}><use xlinkHref="#react"></use></svg>
+    </div>
+  )
+}
+
+export default App
